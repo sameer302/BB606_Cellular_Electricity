09016009e91de0fa5035832d81a87b84aa0f990b
sameer <sameernilkhan11@gmail.com>
2026-02-22 18:14:01 +0530

    added some more explanations

--- Exercise1/Anion based excitable cell membrane/ghk_equation_anion_cl_finding_k_value.py
+++ Exercise1/Anion based excitable cell membrane/ghk_equation_anion_cl_finding_k_value.py
@@ -2,7 +2,7 @@ import numpy as np # math + arrays
 import matplotlib.pyplot as plt # plotting
 
 cl_out = np.linspace(0.1, 1000, 10000) # 10,000 points from 1 to 1000
-k = 4 # Using K here to find what value of Pcl will give us the negative potential peak as -60mV
+k = 4 # Using k here to find what value of Pcl will give us the negative potential peak as -60mV
 vm = 58*np.log10((1000 + k*5)/(30 + k*cl_out))
 # AFter hit and trial we find that the value of k which gives us the peak negative potential as -60mV is 9. 
 
--- Exercise1/Anion based excitable cell membrane/ghk_equation_anion_nit_nernst_and_ghk.py
+++ Exercise1/Anion based excitable cell membrane/ghk_equation_anion_nit_nernst_and_ghk.py
@@ -4,6 +4,15 @@ import matplotlib.pyplot as plt # plotting
 nit_out = np.linspace(0.1, 1000, 10000) # 10,000 points from 1 to 1000
 vm = 58*np.log10(1020/(50*nit_out + 280))
 enit = 58*np.log10(20/nit_out)
+'''
+For Nitrate ion the deviation term at high concentrations is very small hence we don't see deviation here. 
+We know that at very high concentration of external anion, both GHK and Nernst should give same result. So we can calculate the value of 
+membrane potential at very high concentration using both equations and the difference will be our deviation term.
+So for no3- ion, at very high concentration (say 1000 mM):
+Using GHK: Vm = 58 * log10( (1020) / (280 + 50*1000) ), neglecting 280 here, Vm = 58 * log10(1020/50*1000) ~ 58 * log10(20.4/1000) = 58 * log10(20/1000) + 58 * log10(1.02)
+Using Nernst: Vm = 58 * log10(20/1000)
+So the deviation term = + 58 * log10(1.02) = 0.5 which is very small and hence we can ignore it.
+'''
 
 plt.plot(nit_out, vm)
 plt.plot(nit_out, enit)
